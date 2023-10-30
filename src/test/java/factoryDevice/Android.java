963c49b20cd0fe2e5634583ef7af3762563288dc
Katzumi Urdininea <katurdininea02@gmail.com>
2023-10-30 17:09:59 -0400

    Clase

---
+++ src/test/java/factoryDevice/Android.java
@@ -0,0 +1,31 @@
+package factoryDevice;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class Android implements IDevice{
+    @Override
+    public AppiumDriver create(String appPackage, String appActivity) {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName","UPB2023_Android92");
+        capabilities.setCapability("platformVersion","9.0");
+        capabilities.setCapability("appPackage",appPackage);
+        capabilities.setCapability("appActivity",appActivity);
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("automationName","uiautomator2");
+
+        AndroidDriver device = null;
+        try {
+            device = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        device.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        return device;
+    }
+}
